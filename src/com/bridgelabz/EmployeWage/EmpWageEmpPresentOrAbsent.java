900a4e295fc5372a2c3fc4de63b8beb7e3d6b5ed
shashi <shashidharngg@gmail.com>
2022-08-17 16:10:07 +0530

    added:emp daily wage

--- src/com/bridgelabz/EmployeWage/EmpWageEmpPresentOrAbsent.java
+++ src/com/bridgelabz/EmployeWage/EmpWageEmpPresentOrAbsent.java
@@ -4,14 +4,12 @@ public class EmpWageEmpPresentOrAbsent {
 
 	public static void main(String[] args) {
 		int is_full_time = 1;
-		double empCheck = Math.floor(Math.random()*10)%2;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (is_full_time == empCheck) {
 			System.out.println("Employee is Present=");
 		} else {
 			System.out.println("Employee is Absent");
-		}		
+		}
 	}
 
-
-	}
-
+}

6ca28e9f0f184e1f8f45f8e28546e635d5522229
shashi <shashidharngg@gmail.com>
2022-08-17 13:13:30 +0530

    Welcome to employee wage

--- src/com/bridgelabz/EmployeWage/HelloEmployee.java
+++ src/com/bridgelabz/EmployeWage/HelloEmployee.java
@@ -3,8 +3,7 @@ package com.bridgelabz.EmployeWage;
 public class HelloEmployee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to Employee Wage");
 	}
 
 }

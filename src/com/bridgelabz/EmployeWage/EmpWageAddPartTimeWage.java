9c680c2c8cd5335a4df8bb261acdd0faa8ba8e6a
shashi <shashidharngg@gmail.com>
2022-08-17 16:16:28 +0530

    added:Part Time Employee

---
+++ src/com/bridgelabz/EmployeWage/EmpWageAddPartTimeWage.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.EmployeWage;
+
+public class EmpWageAddPartTimeWage {
+
+	public static void main(String[] args) {
+		int isPartTime=1;
+		int isFullTime=2;
+		int empRatePerHour=20;
+		int empHrs;
+		double dailyEmpWage;
+		double empCheck = Math.floor(Math.random()*10)%3;
+		if (isFullTime == empCheck) {
+			empHrs=8;
+		} else if(isPartTime==empCheck){
+			empHrs=4;
+		} else {
+			empHrs=0;
+		}
+		dailyEmpWage=(empHrs*empRatePerHour);
+		System.out.println("Daily Employee Wage="+dailyEmpWage);
+
+	}
+
+	}
+
+
